23637bde6fb312a8e7aafe4558d130ed8c5e6303
harizyusoff2005 <harizyusoff9338@gmail.com>
2024-06-08 22:32:21 +0800

    change the button colour

--- Currency Converter.py
+++ Currency Converter.py
@@ -15,22 +15,22 @@ window.geometry("500x360")
 window.title("Pennywise Converter")
 window.configure(bg='white')  # Set background color to white
 
-label = tk.Label(window, text="Currency Converter", font="Times 20 bold")
+label = tk.Label(window, text="Currency Converter", font="Times 20 bold" , bg='#6dbd81')
 label.place(x=120, y=40)
 
-label1 = tk.Label(window, text="Enter amount here:", font="Times 16 bold")
+label1 = tk.Label(window, text="Enter amount here:", font="Times 16 bold" , bg ='#6dbd81') 
 label1.place(x=70, y=100)
 entry1 = tk.Entry(window)
 
-label2 = tk.Label(window, text="Enter your currency here:", font="Times 16 bold")
+label2 = tk.Label(window, text="Enter your currency here:", font="Times 16 bold", bg ='#6dbd81')
 label2.place(x=30, y=150)
 entry2 = tk.Entry(window)
 
-label3 = tk.Label(window, text="Enter your desired currency:", font="Times 16 bold")
+label3 = tk.Label(window, text="Enter your desired currency:", font="Times 16 bold", bg ='#6dbd81')
 label3.place(x=15, y=200)
 entry3 = tk.Entry(window)
 
-button = tk.Button(window, text="click", font="Times 16 bold", command=clicked)
+button = tk.Button(window, text="click", font="Times 16 bold", command=clicked, bg ='#6dbd81')
 button.place(x=220, y=250)
 
 entry1.place(x=270, y=105)
@@ -38,7 +38,7 @@ entry2.place(x=270, y=155)
 entry3.place(x=270, y=205)
 
 # Create label for displaying result
-label4 = tk.Label(window, text="", font="Times 16 bold")
+label4 = tk.Label(window, text="", font="Times 16 bold", bg ='#6dbd81')
 label4.place(x=200, y=300)
 
 window.mainloop()
